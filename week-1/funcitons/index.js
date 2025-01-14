da0571ad4dbfa4bd7f22527ce08d8bcdd5e40e88
auscohen-IU <auscohen@iu.edu>
2025-01-14 17:26:59 -0500

    Everything Pushed

---
+++ week-1/funcitons/index.js
@@ -0,0 +1,12 @@
+console.log("Math");
+
+function quadraticFormula(a, b, c) {
+    const sqrtParts = b**2 - 4 * a * c;
+    const plusParts = -b + Math.sqrt(sqrtParts);
+    const minusParts = -b - Math.sqrt(sqrtParts);
+
+    return [plusParts / (2 * a), minusParts / (2 * a)];
+}
+
+console.log(quadraticFormula(1, 2, 3));
+
--- week-1/index.js
+++ week-1/index.js
@@ -1,2 +1,8 @@
+const mathFunctions = require('./funcitons');
+
 // Window refers to the browser window; Node.js doesn't have a browser, so it doesn't work. 
 // console.log(window);
+
+console.log("Help!!!");
+
+console.log(mathFunctions.quadraticFormula(1, 2, 1));
